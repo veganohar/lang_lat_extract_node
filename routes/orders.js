dd3cd778a846bf46e82b46d7a0c30d0f26eb6c68
Anantha Lakshmi <narisettyanathalakshmi8@gmail.com>
2025-12-19 11:53:34 +0530

    showing payment and order status, updating payment status

--- routes/orders.js
+++ routes/orders.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/getOrders", async (req, res) => {
   try {
-    const range = "Orders!A2:S"
+    const range = "Orders!A2:T"
     const rows = await getOrders(range);
     res.json({ data: rows });
   } catch (error) {
@@ -16,7 +16,7 @@ router.get("/getOrders", async (req, res) => {
 
 router.post("/newOrder", async (req, res) => {
   try {
-    const range = "Orders!A:S";
+    const range = "Orders!A:T";
     const rows = await newOrder(range,req.body);
     res.status(200).json({ data: rows, status:200, message:"New Order Created Successfully!" });
   } catch (error) {
--- services/orders.js
+++ services/orders.js
@@ -1,18 +1,18 @@
 import { readFile } from "fs/promises";
-import { writeToSheet, readSheetinSequence, deleteRow, deleteRows } from "../utils/readWriteSheetsUtil.js";
+import { writeToSheet, readSheetinSequence, deleteRow, deleteRows, updateRow } from "../utils/readWriteSheetsUtil.js";
 
 const config = JSON.parse(await readFile(new URL("../config/config.json", import.meta.url)));
 const CUSTOMERSSHEET_ID = config.customersSheetId;
 export async function getOrders(range) {
     const data = await readSheetinSequence(range, CUSTOMERSSHEET_ID);
-    const ordersData = data.map(([name, phone, address, mapUrl, latLng, curd, cc, ce, eb, em, ev, gc, lc, mc, ss, amount, comments,distance], index) => (
-        { id: index + 1, name, phone, address, mapUrl, latLng, curd: +curd || 0, cc: +cc || 0, ce: +ce || 0, eb: +eb || 0, em: +em || 0, ev: +ev || 0, gc: +gc || 0, lc: +lc || 0, mc: +mc || 0, ss: +ss || 0, amount: +amount || 0, comments,distance:Number(distance) }
+    const ordersData = data.map(([name, phone, address, mapUrl, latLng, curd, cc, ce, eb, em, ev, gc, lc, mc, ss, amount, comments,distance,payment,status], index) => (
+        { id: index + 1, name, phone, address, mapUrl, latLng, curd: +curd || 0, cc: +cc || 0, ce: +ce || 0, eb: +eb || 0, em: +em || 0, ev: +ev || 0, gc: +gc || 0, lc: +lc || 0, mc: +mc || 0, ss: +ss || 0, amount: +amount || 0, comments,distance:Number(distance), payment:Number(payment),status:Number(status) }
     ));
     return ordersData;
 }
 
 export async function newOrder(range, oData) {
-    const keys = ["name", "phone", "address", "mapUrl", "latLng", "curd", "cc", "ce", "eb", "em", "ev", "gc", "lc", "mc", "ss", "amount", "comments", "distance"]
+    const keys = ["name", "phone", "address", "mapUrl", "latLng", "curd", "cc", "ce", "eb", "em", "ev", "gc", "lc", "mc", "ss", "amount", "comments", "distance","payment","status"]
     const ordData = [];
     for (let key of keys) {
         ordData.push(oData[key]);
@@ -22,12 +22,12 @@ export async function newOrder(range, oData) {
 }
 
 export async function updateOrder(oData, rowNumber) {
-    const keys = ["name", "phone", "address", "mapUrl", "latLng", "curd", "cc", "ce", "eb", "em", "ev", "gc", "lc", "mc", "ss", "amount", "comments", "distance"]
+    const keys = ["name", "phone", "address", "mapUrl", "latLng", "curd", "cc", "ce", "eb", "em", "ev", "gc", "lc", "mc", "ss", "amount", "comments", "distance","payment","status"]
     const ordData = [];
     for (let key of keys) {
         ordData.push(oData[key]);
     }
-    const range = `Orders!A${rowNumber}:S${rowNumber}`
+    const range = `Orders!A${rowNumber}:T${rowNumber}`
     const response = await updateRow(CUSTOMERSSHEET_ID, range, ordData);
     return response;
 }
