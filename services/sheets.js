2ded2ce7523d70c4887776032e583782c3020288
Anantha Lakshmi <narisettyanathalakshmi8@gmail.com>
2026-02-09 21:27:28 +0530

    api adde for pricings

--- services/sheets.js
+++ services/sheets.js
@@ -5,12 +5,12 @@ import { getTwoWheelerDistances } from "../utils/findDistanceUtil.js";
 
 const config = JSON.parse(await readFile(new URL("../config/config.json", import.meta.url)));
 const CUSTOMERSSHEET_ID = config.customersSheetId;
-
+const SALESSHEET_ID = config.salesSheetId;
 // Write Lat and Lng to sheet
 export async function writeLatLng(range) {
   const shortURLs = await readSheetinSequence("Sheet1!D2:D", CUSTOMERSSHEET_ID);
   const latLngs = await processUrls(shortURLs);
-  await clearData(range,CUSTOMERSSHEET_ID);
+  await clearData(range, CUSTOMERSSHEET_ID);
   return await writeToSheet(range, latLngs, CUSTOMERSSHEET_ID);
 }
 
@@ -42,16 +42,29 @@ export async function readSheet(range) {
 
 
 export async function synchDistances(range) {
-    const data = await readSheetinSequence(range, CUSTOMERSSHEET_ID);
-    const coords = data.map(item => item[0]);
-    const distancesData = await getTwoWheelerDistances(coords);
-    const distances = distancesData.map(item => item.distanceMeters);
-    const writeRange = "Sheet1!F2:F";
-    await clearData(writeRange, CUSTOMERSSHEET_ID);
-    return await writeToSheet(writeRange, distances, CUSTOMERSSHEET_ID);
+  const data = await readSheetinSequence(range, CUSTOMERSSHEET_ID);
+  const coords = data.map(item => item[0]);
+  const distancesData = await getTwoWheelerDistances(coords);
+  const distances = distancesData.map(item => item.distanceMeters);
+  const writeRange = "Sheet1!F2:F";
+  await clearData(writeRange, CUSTOMERSSHEET_ID);
+  return await writeToSheet(writeRange, distances, CUSTOMERSSHEET_ID);
 }
 
-
+export async function getPrices() {
+  const data = await readSheetinSequence("Pricing!A3:H", SALESSHEET_ID);
+  const prices = data.map((item, i) => ({
+    id: i + 1,
+    name: item[0],
+    shortName: item[1],
+    sizes: {
+      "100ml": { mrp: +item[2], sellingPrice: +item[5] },
+      "500ml": { mrp: +item[3], sellingPrice: +item[6] },
+      "4L": { mrp: +item[4], sellingPrice: +item[7] }
+    }
+  }));
+  return prices;
+}
 
 
 
